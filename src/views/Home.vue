dc7754fb0ff31c40f13a818c356344ba25028f4d
zarlinaingtun <zarlinaingtun1999@gmail.com>
2021-08-16 22:14:06 -0700

    ref() for reactivity

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,24 @@
 <template>
   <h1>Home</h1>
   <h2>My name is {{name}}.Age is {{age}}</h2>
+
+  <input type="text" v-model="name"/>
   <button @click="handleClick">Click</button>
+  <button @click="age++">Increase Age</button>
 </template>
 
 <script>
+import { ref } from '@vue/reactivity';
 
 
 export default {
   setup(){
-    let name="Zar Li";
-    let age=21;
-    console.log(name);
+    let name=ref('Zar Li');
+    let age=ref(21);
+    console.log(name.value);
 
     let handleClick=()=>{
-      console.log('U click me');
+      name.value="Ma Ma";
     }
     return {name,age,handleClick};
   }
