bd0733b9b15cdcc1fdc4cfd7acd4f8ffc7232ad0
zarlinaingtun <zarlinaingtun1999@gmail.com>
2021-08-17 22:14:23 -0700

    computed property in setup funtion

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -25,14 +25,24 @@
   <p>City Two-{{citytwo}}</p>
   <button @click="cityTwo">Click</button>
 
+  <hr>
+  <p>Computed Property</p>
+  <p>Search Data {{search}}</p>
+  <input type="text" v-model="search"/>
+  <div v-for="username in filterusernames" :key="username">
+    <p>{{username}}</p>
+  </div>
+
 </template>
 
 <script>
-import { reactive, ref } from '@vue/reactivity';
+import { computed, reactive, ref } from '@vue/reactivity';
 
 
 export default {
   setup(){
+    let usernames=ref(["Mg Mg","Su Su","Aye Aye","Kyaw Kyaw","Aung"]);
+    let search=ref('');
     let name=ref('Zar Li');//reactive value
     let age=ref(21);
     console.log(name.value);
@@ -66,10 +76,18 @@ export default {
     let citytwo=reactive("Japan");
     console.log(citytwo);
     let cityTwo=()=>{
-      citytwo="Noway";
+      // citytwo="Noway";
     }
 
-     return {name,age,handleClick,personOne,changeOne,personTwo,changeTwo,cityone,cityOne,citytwo,changeTwo};
+    //Computed Property
+    let filterusernames=computed(()=>{
+      return usernames.value.filter(username=>{
+        let smallusername=username.toLowerCase();
+        let smallsearch=search.value.toLowerCase();
+        return smallusername.includes(smallsearch);
+      })
+    })
+     return {name,age,handleClick,personOne,changeOne,personTwo,changeTwo,cityone,cityOne,citytwo,changeTwo,usernames,search,filterusernames};
   }
   
 }
